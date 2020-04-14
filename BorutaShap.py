92d0751a71c6dcd66509e5a3ac31c5b3acf8c2fe
Eoghan <egnkeany@gmail.com>
2020-04-14 21:51:32 +0100

    Added model and data quality checks

--- BorutaShap.py
+++ BorutaShap.py
@@ -6,65 +6,64 @@ import shap
 
 class BorutaShap:
 
-
     def __init__(self, model=None, Shap=True):
 
         self.Shap = Shap
+        self.model = model
+        self.check_model()
 
-        if isinstance(model, RandomForestClassifier):
-            self.model = model
+
+    def check_model(self):
+
+        check_fit = hasattr(self.model, 'fit')
+        check_predict_proba = hasattr(self.model, 'predict_proba')
+
+        if self.model is None:
+            self.model = RandomForestClassifier()
+        
+        elif check_fit is False and check_predict_proba is False:
+            raise AttributeError('Model must contain both the fit() and predict_proba() methods')
 
         else:
-            self.model = RandomForestClassifier()
+            pass
 
-    
+
+    def check_X(self):
+
+        if isinstance(self.X, pd.DataFrame) is False:
+            raise AttributeError('X must be a pandas Dataframe')
+        else:
+            pass 
+
+
     def fit(self, X, y, n_trials = 20):
         
         self.X = X
         self.y = y
         
         self.check_X()
-        self.x_boruta = self.create_shadow_features()
-        self.model.fit(self.x_boruta, self.y)
+        self.create_shadow_features()
+        self.model.fit(self.X_boruta, self.y)
+        X_feature_import, Shadow_feature_import = self.feature_importance()
 
-
-        pass
 
     
     def create_shadow_features(self):
 
-        if self.x_type == "pandas":
-
             self.X_shadow = self.X.apply(np.random.permutation)
             self.X_shadow.columns = ['shadow_' + feature for feature in self.X.columns]
-            X_boruta = pd.concat([self.X, X_shadow], axis = 1)
-
-        else:
-            pass
-
-        return X_boruta
-    
-
-    def check_X(self):
-
-        if isinstance(self.X, pd.DataFrame):
-            self.x_type == "pandas"
-
-        elif isinstance(self.X, np.array):
-            self.x_type == "numpy"
-
-        else:
-            raise AttributeError('X must be a pandas Dataframe or Numpy array')
+            self.X_boruta = pd.concat([self.X, self.X_shadow], axis = 1)
 
 
     def feature_importance(self):
 
         if self.Shap:
 
-            shap_values = self.explainer()
+            self.explainer()
             
-            vals = np.abs(shap_values).mean(0)
-            feature_importance = pd.DataFrame(list(zip(self.x_boruta.columns,vals)), columns=['col_name','feature_importance_vals'])
+            vals = np.abs(self.shap_values).mean(0)
+            feature_importance = pd.DataFrame(list(zip(self.x_boruta.columns,vals)),
+                                              columns=['col_name','feature_importance_vals'])
             X_feature_import = feature_importance[self.X.columns]
             Shadow_feature_import = feature_importance[self.X_shadow.columns]
 
@@ -77,10 +76,13 @@ class BorutaShap:
 
     def explainer(self):
         explainer = shap.TreeExplainer(self.model)
-        shap_values = explainer.shap_values(self.x_boruta)
-        return shap_values
+        self.shap_values = explainer.shap_values(self.x_boruta)
+        
 
 
+if __name__ == "__main__":
 
+    forest = RandomForestClassifier()
+    BorutaShap(model=forest)
 
 
