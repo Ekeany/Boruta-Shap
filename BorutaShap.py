b13c736516e4ffc4937c0e63d67811acbf3870d2
Eoghan <egnkeany@gmail.com>
2020-04-18 17:36:27 +0100

    Added Feature Importance and Regression

--- BorutaShap.py
+++ BorutaShap.py
@@ -1,14 +1,19 @@
-from sklearn.ensemble import RandomForestClassifier
-import numpy as np
+from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
 import pandas as pd
+import numpy as np
 import shap
+import os
 
+import warnings
+warnings.filterwarnings("ignore")
 
 class BorutaShap:
 
-    def __init__(self, model=None, Shap=True, model_type = 'tree'):
-
+    def __init__(self, model=None, Shap=True, model_type = 'tree', classification = True):
+        
         self.Shap = Shap
+        self.classification = classification
+        self.model = model
         self.model_type = model_type
         self.check_model()
 
@@ -16,22 +21,26 @@ class BorutaShap:
     def check_model(self):
 
         check_fit = hasattr(self.model, 'fit')
-        check_predict_proba = hasattr(self.model, 'predict_proba')
+        check_predict_proba = hasattr(self.model, 'predict')
 
         if self.model is None:
-            self.model = RandomForestClassifier()
+            if self.classification:
+                self.model = RandomForestClassifier()
+            else:
+                self.model = RandomForestRegressor()
         
         elif check_fit is False and check_predict_proba is False:
-            raise AttributeError('Model must contain both the fit() and predict_proba() methods')
+            raise AttributeError('Model must contain both the fit() and predict() methods')
 
         else:
-            self.model = model
+            pass
 
 
     def check_X(self):
 
         if isinstance(self.X, pd.DataFrame) is False:
             raise AttributeError('X must be a pandas Dataframe')
+
         else:
             pass
 
@@ -58,6 +67,7 @@ class BorutaShap:
         self.create_shadow_features()
         self.model.fit(self.X_boruta, self.y)
         X_feature_import, Shadow_feature_import = self.feature_importance()
+        print(X_feature_import > Shadow_feature_import.max())
 
 
     
@@ -72,33 +82,52 @@ class BorutaShap:
 
         if self.Shap:
 
-            self.explainer()
-            
+            self.explain()
             vals = np.abs(self.shap_values).mean(0)
-            feature_importance = pd.DataFrame(list(zip(self.x_boruta.columns,vals)),
-                                              columns=['col_name','feature_importance_vals'])
-            X_feature_import = feature_importance[self.X.columns]
-            Shadow_feature_import = feature_importance[self.X_shadow.columns]
+
+            X_feature_import = vals[:len(self.X.columns)]
+            Shadow_feature_import = vals[len(self.X_shadow.columns):]
 
         else:
+
             X_feature_import = self.model.feature_importances_[:len(self.X.columns)]
             Shadow_feature_import = self.model.feature_importances_[len(self.X.columns):]
 
         return X_feature_import, Shadow_feature_import
 
 
-    def explainer(self):
+    def explain(self):
 
         if self.model_type == 'tree':
             explainer = shap.TreeExplainer(self.model)
-            self.shap_values = explainer.shap_values(self.x_boruta)
-        elif
-        
+            
+            if self.classification:
+                # for some reason shap returns values wraped in a list of length 1
+                self.shap_values = explainer.shap_values(self.X_boruta)[0]
+            else:
+                self.shap_values = explainer.shap_values(self.X_boruta)
+
+        elif self.model_type == 'linear':
+            explainer = shap.LinearExplainer(self.model, self.X_boruta, feature_dependence="independent")
+            self.shap_values = explainer.shap_values(self.X_boruta)
+
+        else:
+            raise AttributeError("Model Type has not been Selected (linear or tree)")
+            
 
 
 if __name__ == "__main__":
+    
+    np.random.seed(56)
+    current_directory = os.getcwd()
 
-    forest = RandomForestClassifier()
-    BorutaShap(model=forest)
+    X = pd.read_csv(current_directory + '\\Datasets\\Ozone.csv')
+    y = X.pop('V4')
+
+    feature_selector = BorutaShap(Shap=False, classification=False)
+    feature_selector.fit(X, y)
+
+    feature_selector = BorutaShap(Shap=True, classification=False)
+    feature_selector.fit(X, y)
 
 
