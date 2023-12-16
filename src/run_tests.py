1329330fdd3f6f5344a03f8d9adce52fcdd8a22e
craut <chinmay0raut@gmail.com>
2023-12-15 20:29:15 -0500

    added histogram gradient boosting trees to run_tests.py

--- src/run_tests.py
+++ src/run_tests.py
@@ -5,7 +5,11 @@ from sklearn.tree import DecisionTreeClassifier, DecisionTreeRegressor
 from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
 from lightgbm import LGBMClassifier, LGBMRegressor
 
-
+# in sklearn.__version__ < 1.0, HistGradientBoostingClassifier is an experimental feature & is not fully supprted by shap
+from sklearn import __version__ as sklearn_version
+from distutils.version import StrictVersion
+if StrictVersion(sklearn_version) >= StrictVersion("1.0.0"):
+    from sklearn.ensemble import HistGradientBoostingRegressor ,HistGradientBoostingClassifier
 
 def Test_Models(data_type, models):
 
@@ -41,6 +45,9 @@ if __name__ == "__main__":
                        'xgboost-regressor':XGBRegressor(),'lightgbm-regressor':LGBMRegressor(),
                        'catboost-regressor':CatBoostRegressor()}
 
+    if StrictVersion(sklearn_version) >= StrictVersion("1.0.0"):
+        tree_classifiers['histgradientboosting-classifier'] = HistGradientBoostingClassifier()
+        tree_regressors['histgradientboosting-regressor'] = HistGradientBoostingRegressor()
     
     Test_Models('regression', tree_regressors)
     Test_Models('classification', tree_classifiers)
